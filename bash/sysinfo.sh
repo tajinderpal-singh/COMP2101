6b4f35cfe13e417ae30b8e321e79bb6af26f09ac
Tajinderpal Singh <tajinderpalsinghvirk22@gmail.com>
2023-02-15 11:14:47 -0500

    system information

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -2,23 +2,37 @@
 # This is a script to display the computer information.
 
 #System's fully-qualified domain name (FQDN)
-echo "Fully-qualified domain name(FQDN) of system: $(hostname -f)"
+fqdn=$(hostname -f)
 
 # Host information including operating system name and version
-echo " Important host information is as follow:"
-hostnamectl
+hostname=$(hostname)
+operatingsystem=$(hostnamectl | grep 'Operating System' | awk '{print $3,$4,$5}')
+version=$(hostnamectl | grep 'Kernel' | awk '{print $2,$3}')
 
-# IP Addresses that the machine has, excluding IP addresses on the 127 network
+# IP Addresses that the machine uses to send and receive data from the interface
 # Output obtained from command ip address is filtered using grep command
 # Then the output from grep comand is manipulated using awk command
-echo "IP Addresses available excluding address on 127 network:"
-ip address |
+IPaddress=$(ip address |
 grep 'inet' |
-grep -v "127" |
+grep '192' |
 awk '{print $2}' |
-cut -f1 -d'/'
+cut -f1 -d'/')
 
 # Amount of space available in the root filesystem
 # The space amount is represented in Human-friendly number
-echo "Root Filesystem Status:"
-  df -H /dev/sda3
+RootFilespace=$(df -H /dev/sda3 |
+awk '{print $4}' |
+sed '1d')
+
+# Summary of the report including all the variables mentioned above
+cat<<EOF
+
+My Report for $hostname
+=========
+FQDN: $fqdn
+Operating System name and version: $operatingsystem/$version
+IP Address: $IPaddress
+Root Filesystem Free Space: $RootFilespace
+=========
+
+EOF
